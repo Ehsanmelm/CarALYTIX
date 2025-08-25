000fcf58ef6c675f9c3d40f4fe61d53ab3849508
rezaebadi <rezaaebadi1381@gmail.com>
2025-08-25 08:42:57 +0330

    change sub domain add changed settings

--- carAlytix/settings.py
+++ carAlytix/settings.py
@@ -196,11 +196,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
 
 CORS_ALLOWED_ORIGINS = [
-    'https://caralytix.darkube.app',
+    'https://caralytix-api.darkube.app',
     # 'https://event-front.darkube.app',
     # 'http://localhost:3000'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://caralytix.darkube.app',
+    'https://caralytix-api.darkube.app',
     # 'https://event-front.darkube.app'
 ]
